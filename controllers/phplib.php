dbcc2d462f2c241d2917b3126ac1c36690a4f603
Ehsan Mohammadi <ehsanx64@gmail.com>
2018-12-27 18:12:54 +0330

    Installed phplib and added a quick test for it

---
+++ controllers/phplib.php
@@ -0,0 +1,18 @@
+<?php
+use ehsanx64\phplib\General\Date;
+
+class PhplibController extends Controller {
+
+    public function __construct() {
+    }
+
+    public function index() {
+        // Is 123 similar to a date?
+        $r = Date::isDateString('123');
+        var_dump($r);
+
+        // What about 2018-01-03?
+        $r = Date::isDateString('2018-01-03');
+        var_dump($r);
+    }
+}
