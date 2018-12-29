bbea8eba5968f2c088c8c5a52e4ea5d9911f6d43
Ehsan Mohammadi <ehsanx64@gmail.com>
2018-12-29 16:20:27 +0330

    php-test index page updated a bit

--- views/site/index.php
+++ views/site/index.php
@@ -2,16 +2,16 @@
 $this->pageTitle = 'Test List';
 ?>
 <h1>Test List</h1>
+
 <h3>phplib</h3>
-<ul>
-	<li><a href="/phplib/test-module">Modules</a></li>
-</ul>
+<h4>General purpose PHP library</h4>
+<a href="/phplib">phplib</a>
+<hr />
 
 <h3>Other</h3>
-<ul>
-	<li><a href="/sabreevent/index">SabreEvent</a></li>
-	<li></li>
-</ul>
+<h4>Miscellaneous tests</h4>
+<p><a href="/sabre-event">SabreEvent</a></p>
+<hr />
 
 <h3>HashCrypt</h3>
 <h4>Hashing and cryptography related tests</h4>
