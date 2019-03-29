7298bc2b5bf66953ba5c0ff0787f5d846df264eb
Ehsan Mohammadi <ehsanx64@gmail.com>
2019-03-29 17:06:41 +0430

    Materialize-boilerplate added to make a better appearance

--- layout/default.php
+++ layout/default.php
@@ -1,10 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title><?php echo $this->pageTitle; ?></title>
+	<meta charset="utf-8">
+	<title><?php echo $this->pageTitle; ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link type="text/css" rel="stylesheet" href="/assets/css/materialize.min.css" media="screen, projection"/>
+	<link type="text/css" rel="stylesheet" href="/assets/css/font-awesome.min.css" media="screen, projection"/>
 </head>
+
 <body>
-	<?php echo $content; ?>
+	<div class="navbar-fixed">
+		<nav>
+			<div class="nav-wrapper">
+				<a href="#!" class="brand-logo">Logo</a>
+				<ul class="right hide-on-med-and-down">
+					<li><a href="sass.html">Sass</a></li>
+					<li><a href="badges.html">Components</a></li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<?php echo $content; ?>
+		</div>
+	</div>
+
+	<footer class="page-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col l6 s12">
+					<h5 class="white-text">Footer Content</h5>
+					<p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+				</div>
+				<div class="col l4 offset-l2 s12">
+					<h5 class="white-text">Links</h5>
+					<ul>
+						<li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+						<li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+						<li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+						<li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="footer-copyright">
+			<div class="container">
+				© 2014 Copyright Text
+				<a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+			</div>
+		</div>
+	</footer>
+
+	<script type="text/javascript" src="/assets/css/js/jquery.min.js"></script>
+	<script type="text/javascript" src="/assets/css/js/materialize.min.js"></script>
 </body>
 </html>
